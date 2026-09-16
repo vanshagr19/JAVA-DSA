54c5f77e52aa43d8955b8edf570bbaf1abcec84f
vanshagr19 <vansh1909agrawal@gmail.com>
2026-09-16 22:03:04 +0530

    Auto‑commit 2026-09-16T16:33:04.585Z

---
+++ strings/StringsBasics.java
@@ -0,0 +1,13 @@
+package strings;
+import java.util.Scanner;
+
+public class StringsBasics {
+    public static void main(String[] args) {
+        
+    
+    Scanner sc = new Scanner(System.in);
+    String s = sc.nextLine();
+    System.out.println(s);
+    }
+}
+ 
