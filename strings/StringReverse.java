a6b6ad36dd59c955d2c682065f517e4c871e6900
vanshagr19 <vansh1909agrawal@gmail.com>
2026-09-17 22:24:06 +0530

    Auto‑commit 2026-09-17T16:54:05.930Z

--- strings/StringReverse.java
+++ strings/StringReverse.java
@@ -2,21 +2,24 @@ package strings;
 
 public class StringReverse {
     public static void main(String[] args) {
-        String st  = "Vansh";
-    StringBuilder s = new StringBuilder(st) ;
+        String st = "Vansh";
+        StringBuilder s = new StringBuilder(st);
 
-    // s.reverse();
+        // s.reverse();
 
-    int i  = 0, j = s.length()-1;
-    while(i<=j){
-        char t1 = s.charAt(i);
-        char t2 = s.charAt(j);
-        s.setCharAt(i, t2);
-        s.setCharAt(j, t1);
-        i++;
-        j--;
+        int i = 0, j = s.length() - 1;
+        // while(i<=j){
+        // char t1 = s.charAt(i);
+        // char t2 = s.charAt(j);
+        // s.setCharAt(i, t2);
+        // s.setCharAt(j, t1);
+        // i++;
+        // j--;
+        // }
+
+        s.deleteCharAt(3);
+        s.insert(3, 's');
+        System.out.println(s);
     }
-    System.out.println(s);
-    }
-    
+
 }
